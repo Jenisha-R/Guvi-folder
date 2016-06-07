f26374939603229e8e85b59bd19c12e14376b957
Jenisha-R <jenisharamanathan95@gmail.com>
2016-06-07 15:06:23 +0530

    Create fib.c

---
+++ fib.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a=0,b=1,c=0, n;
+    scanf("%d",&n);
+    c=a+b;
+    while(c < n)
+    {
+        printf("%d ",c);
+        a=b;
+        b=c;
+        c=a+b;
+    }
+    return 0;
+}
