21a57a8c707ab0f39276de562fffdb837a565b6e
Jenisha-R <jenisharamanathan95@gmail.com>
2016-06-07 12:32:39 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2, i, flag;
+    scanf("%d %d", &n1, &n2);
+    printf("The odd numbers are:");
+    while (n1 < n2)
+    {
+        flag=0;
+        for(i=0;i<n1;i++)
+        {
+            if(n1%2!=0)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if (flag==1)
+            printf("%d ",n1);
+		n1++;
+    }
+
+    return 0;
+}
